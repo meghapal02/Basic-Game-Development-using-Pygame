e6fc59257dc1bc47207bc1ab6f1d637ccec16d8e
Megha Rupesh Pal <62481884+meghapal02@users.noreply.github.com>
2020-10-06 20:29:37 +0530

    Add files via upload
    The python code is extended and I have added a code of moving sprite.

---
+++ Sprites.py
@@ -0,0 +1,73 @@
+#Sprite: A compter graphic term for an object that can move around the screen (eg: Mario)
+
+import pygame
+import random
+
+#Set the window
+WIDTH = 800
+HEIGHT = 600
+
+#FPS: In how much time the screen will be updated
+FPS = 30
+
+#Initialize pygame
+pygame.init()
+
+#Initialize music
+pygame.mixer.init()
+
+#Create window
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+#Set caption
+pygame.display.set_caption("My First Game")
+
+#Make clock
+clock = pygame.time.Clock()
+
+#Define colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+class Player(pygame.sprite.Sprite):
+    #sprite for the player
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((50, 50))
+        self.image.fill(GREEN)
+        self.rect = self.image.get_rect()
+        self.rect.center = (WIDTH / 2, HEIGHT / 2)
+
+    def update(self):
+        #Make it move in x direction
+        self.rect.x += 5
+        #Not let it go out of the screen
+        if self.rect.left > WIDTH:
+            self.rect.right = 0
+
+#A group for all sprites
+all_sprites = pygame.sprite.Group()
+#Player object
+player =  Player()
+all_sprites.add(player)
+#Game loop
+running = True
+while running:
+    #keep loop running at the right speed
+    clock.tick(FPS)
+    #Process input (events) 
+    for event in pygame.event.get():
+        #check for closing window
+        if event.type == pygame.QUIT:
+            running = False
+    #UpdatSe
+    all_sprites.update()
+    #Draw/render
+
+    screen.fill(BLACK)
+    all_sprites.draw(screen)
+    #Double buffering(After drawing everything flip the display)
+    pygame.display.flip()
